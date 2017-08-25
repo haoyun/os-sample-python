774012890a08a8c616632ec82f773a8f1e54c3b7
Yun Hao <haoyun@users.noreply.github.com>
2017-08-25 17:35:38 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,12 @@ import os
 # 导入模块
 from wxpy import *
 # 初始化机器人，扫码登陆
-bot = Bot(console_qr=True, cache_path=True)
+bot = Bot(console_qr=1.1, cache_path=True)
+
+# 给机器人自己发送消息
+bot.self.send('Hello World!')
+# 给文件传输助手发送消息
+bot.file_helper.send('Hello World!')
 
 workers = int(os.environ.get('GUNICORN_PROCESSES', '3'))
 threads = int(os.environ.get('GUNICORN_THREADS', '1'))
